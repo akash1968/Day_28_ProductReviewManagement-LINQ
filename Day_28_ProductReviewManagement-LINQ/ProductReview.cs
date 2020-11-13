6363560ad423e1cd073b3e6f75a50f35d0ff1167
Akash <akash.singh670@gmail.com>
2020-11-13 21:06:32 +0530

    [Akash] .Added Day_28_ProductReviewManagement-LINQ Assignment

---
+++ Day_28_ProductReviewManagement-LINQ/ProductReview.cs
@@ -0,0 +1,25 @@
+﻿
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProductReview.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akash Kumar Singh"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_28_ProductReviewManagement_LINQ
+{
+    /// <summary>
+    /// UC 1 : Class having mentioned properties
+    /// </summary>
+   public class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
